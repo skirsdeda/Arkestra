bbb23b73a9188183f11fff55c8202e9f227d7c8f
Daniele Procida <daniele@vurt.org>
2014-02-14 16:11:06 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
         'django-pagination',
         'django-easyfilters',
         'django-treeadmin',
+        'django-widgetry',
     ],
     dependency_links=[
         'https://github.com/evildmp/django-widgetry/archive/master.zip#egg=django-widgetry'
